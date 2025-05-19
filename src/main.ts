10cea99257c25026a66ae579cca63283876443b6
MartinLens <j.martin.b1989@gmail.com>
2025-05-19 13:31:19 -0300

    feat: se añade cookie-parser para gestionar cookies en la aplicación

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  app.use(cookieParser());
 
   // Habilitar CORS
   app.enableCors({
